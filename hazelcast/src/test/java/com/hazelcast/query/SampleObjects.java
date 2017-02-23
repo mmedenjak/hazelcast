1da35a9282abc1b601f1d9db73d3ceaed9b43499
Matko Medenjak <matko@hazelcast.com>
2017-02-23 15:38:07 +0100

    portable performance test

--- hazelcast/src/test/java/com/hazelcast/query/SampleObjects.java
+++ hazelcast/src/test/java/com/hazelcast/query/SampleObjects.java
@@ -65,6 +65,22 @@ public final class SampleObjects {
             age = reader.readInt("a");
         }
 
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
         @Override
         public String toString() {
             return "PortableEmployee{"
